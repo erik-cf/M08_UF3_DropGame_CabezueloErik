f5a66dbeb3f7a48146f651109275df8845b871c5
erik-cf <erikcf95@gmail.com>
2020-01-16 16:38:15 +0100

    Power Ups acabats, falten ultims retocs

--- core/src/com/ecabezue/drop/GameScreen.java
+++ core/src/com/ecabezue/drop/GameScreen.java
@@ -9,16 +9,17 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.TimeUtils;
-
 import java.util.Iterator;
 
 public class GameScreen implements Screen {
+    private Texture goldenDropImage;
     private Texture dropImage;
     private Texture bucketImage;
     private Sound dropSound;
@@ -28,20 +29,49 @@ public class GameScreen implements Screen {
     private Rectangle bucket;
     private Array<Rectangle> raindrops;
     private long lastDropTime;
+    private long lastPowerUpTime;
     private BitmapFont puntuacio;
+    private BitmapFont fps;
+    private BitmapFont actualPowerUp;
     private int contador;
-    private float speed = 3;
+    private float speed = 4;
     final Drop game;
-    public BitmapFont introText;
+    private boolean powerUpSpawned = false;
+    private long timePowerUp;
+    private long maxPowerUpTime;
+    private Texture dropGamebg;
+    private Array<Rectangle> powerUps;
+    private Array<Rectangle> goldenDrops;
+    private Texture powerUp;
+    private Sprite sprPowerUp;
+    private Sprite sprGoldenDrop;
+    private boolean powerUpBucketBig = false;
+    private boolean powerUpBucketTiny = false;
+    private boolean powerUpInvertControl = false;
+    private boolean powerUpGoldenDrop = false;
+
+    private String activePowerUp = "No hi ha cap poder equipat...";
 
     public GameScreen(final Drop game) {
         this.game = game;
-        // Carreguem les imatges del cub i de la gota
+        // Carreguem les imatges del cub, de la gota, dels powerups, del background i del goldendrop
         dropImage = new Texture(Gdx.files.internal("droplet.png"));
         bucketImage = new Texture(Gdx.files.internal("bucket.png"));
+        goldenDropImage = new Texture(Gdx.files.internal("goldendrop.png"));
+        dropGamebg = new Texture(Gdx.files.internal("dropgamebg.jpg"));
+        powerUp = new Texture(Gdx.files.internal("powerup.png"));
 
+        // Inicialitzem els sprites (per redimensionar)
+        sprPowerUp = new Sprite(powerUp);
+        sprPowerUp.setSize(64, 64);
+
+        sprGoldenDrop = new Sprite(goldenDropImage);
+        sprGoldenDrop.setSize(64, 64);
+
+        // Inicialitzem les fonts
         puntuacio = new BitmapFont();
-
+        fps = new BitmapFont();
+        actualPowerUp = new BitmapFont();
 
         // Carreguem el só
         dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.wav"));
@@ -64,17 +94,20 @@ public class GameScreen implements Screen {
         bucket = new Rectangle();
         bucket.x = 800 / 2 - 64 / 2;
         bucket.y = 20;
+
         // Li setegem width i height
         bucket.width = 64;
         bucket.height = 64;
 
-        if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) bucket.x -= 200 * Gdx.graphics.getDeltaTime();
-        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) bucket.x += 200 * Gdx.graphics.getDeltaTime();
+        // Inicialitzem els arrays
+        powerUps = new Array<Rectangle>();
+        goldenDrops = new Array<Rectangle>();
+        raindrops = new Array<Rectangle>();
 
         if(bucket.x < 0) bucket.x = 0;
         if(bucket.x > 800 - 64) bucket.x = 800 - 64;
 
-        raindrops = new Array<Rectangle>();
+
         spawnRaindrop();
     }
 
@@ -90,47 +123,164 @@ public class GameScreen implements Screen {
         // Indiquem que el batch utilitzi el sistema de coordenades de la camera
         batch.setProjectionMatrix(camera.combined);
         batch.begin();
-        puntuacio.draw(batch, "Punts: " + contador, 650, 370);
-        batch.draw(bucketImage, bucket.x, bucket.y);
-        for(Rectangle raindrop: raindrops) {
-            batch.draw(dropImage, raindrop.x, raindrop.y);
+        batch.draw(dropGamebg, 0, 0, 800, 480);
+        if(powerUpBucketBig){
+            bucket.width = 128;
+            bucket.height = 128;
+            batch.draw(bucketImage, bucket.x, bucket.y, 128, 128);
+        }else if(powerUpBucketTiny){
+            bucket.width = 32;
+            bucket.height = 32;
+            batch.draw(bucketImage, bucket.x, bucket.y, 32, 32);
+        }else {
+            bucket.width = 64;
+            bucket.height = 64;
+            batch.draw(bucketImage, bucket.x, bucket.y);
+        }
+
+        actualPowerUp.draw(batch, activePowerUp, 300, 460);
+        fps.draw(batch, "fps: " + Gdx.graphics.getFramesPerSecond(), 10, 460);
+        puntuacio.draw(batch, "Punts: " + contador, 720, 460);
+
+        for (Rectangle pow : powerUps) {
+            batch.draw(sprPowerUp, pow.x, pow.y, 64, 64);
+        }
+        if(powerUpGoldenDrop) {
+            for (Rectangle goldendrop : goldenDrops) {
+                batch.draw(sprGoldenDrop, goldendrop.x, goldendrop.y, 64, 64);
+            }
+        }else {
+            for(Rectangle raindrop: raindrops) {
+                batch.draw(dropImage, raindrop.x, raindrop.y);
+            }
         }
         batch.end();
 
         // Comprovem si es toca la pantalla o es clica el mouse
         if(Gdx.input.isTouched()) {
-            // Guardarem la posició a un vector
-            Vector3 touchPos = new Vector3();
-            touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
-            // Transformem el sistema de coordenades al de la camera
-            camera.unproject(touchPos);
-            // Fiquem les coordenades al bucket
-            bucket.x = touchPos.x - 64 / 2;
+            if(powerUpInvertControl){
+                // Guardarem la posició a un vector
+                Vector3 touchPos = new Vector3();
+                touchPos.set((800 / 2 )- (Gdx.input.getX() / 1.5f), -Gdx.input.getY(), 0);
+                // Transformem el sistema de coordenades al de la camera
+                camera.unproject(touchPos);
+                // Fiquem les coordenades al bucket
+                bucket.x = touchPos.x - 64 / 2;
+            }else{
+                // Guardarem la posició a un vector
+                Vector3 touchPos = new Vector3();
+                touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+                // Transformem el sistema de coordenades al de la camera
+                camera.unproject(touchPos);
+                // Fiquem les coordenades al bucket
+                bucket.x = touchPos.x - 64 / 2;
+            }
         }
 
-        if(TimeUtils.nanoTime() - lastDropTime > 1000000000) spawnRaindrop();
-        //speed = 200 * Gdx.graphics.getDeltaTime();
+        if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            if(powerUpInvertControl) {
+                bucket.x += 400 * Gdx.graphics.getDeltaTime();
+            }else{
+                bucket.x -= 400 * Gdx.graphics.getDeltaTime();
+            }
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            if(powerUpInvertControl){
+                bucket.x -= 400 * Gdx.graphics.getDeltaTime();
+            }else{
+                bucket.x += 400 * Gdx.graphics.getDeltaTime();
+            }
+        }
 
-        for (Iterator<Rectangle> iter = raindrops.iterator(); iter.hasNext(); ) {
+        if((TimeUtils.nanoTime() - lastPowerUpTime) > 2000000000l){
+            powerUpSpawned = false;
+        }
 
-            Rectangle raindrop = iter.next();
-            raindrop.y -= speed;
-            if(raindrop.overlaps(bucket) && raindrop.y > (70 - speed)) {
+        if((TimeUtils.nanoTime() - timePowerUp) > 5000000000l) {
+            powerUpBucketBig = false;
+
+        }
+
+        if((TimeUtils.nanoTime() - timePowerUp) > 8000000000l) {
+            powerUpBucketTiny = false;
+
+        }
+
+        if((TimeUtils.nanoTime() - timePowerUp) > 3000000000l) {
+            powerUpInvertControl = false;
+
+        }
+
+        if((TimeUtils.nanoTime() - timePowerUp) > 12000000000l) {
+            powerUpGoldenDrop = false;
+            goldenDrops.clear();
+
+        }
+
+        if(!powerUpBucketBig && !powerUpBucketTiny && !powerUpInvertControl && !powerUpGoldenDrop) {
+            resetPowerUpString();
+        }
+
+        if(TimeUtils.nanoTime() - lastDropTime > 2000000000l && !powerUpSpawned) {
+            if(powerUpGoldenDrop) {
+                spawnGoldenDrop();
+            }else{
+                spawnRaindrop();
+            }
+        }
+
+        maxPowerUpTime = (long)((Math.random() * 18000000000l) + 9000000000l);
+
+        if(TimeUtils.nanoTime() - lastPowerUpTime > maxPowerUpTime) spawnPowerUp();
+
+        for (Iterator<Rectangle> iter = powerUps.iterator(); iter.hasNext(); ) {
+            Rectangle powerUp = iter.next();
+            powerUp.y -= speed;
+            if(powerUp.overlaps(bucket) && powerUp.y > (70 - speed)) {
                 dropSound.play();
                 iter.remove();
-                contador++;
-                if(contador % 10 == 0){
-                    speed *= 1.2f;
+                contador+=10;
+                compAccio((int)((Math.random() * 4) + 1));
+            }
+            if(powerUp.y + 64 < 0) {
+                iter.remove();
+            }
+        }
+
+        if(powerUpGoldenDrop){
+            raindrops.clear();
+            for (Iterator<Rectangle> iter = goldenDrops.iterator(); iter.hasNext(); ) {
+
+                Rectangle raindrop = iter.next();
+                raindrop.y -= speed;
+                if(raindrop.overlaps(bucket) && raindrop.y > ((bucket.height - 14) - speed)) {
+                    dropSound.play();
+                    iter.remove();
+                    contador+=5;
+                }
+                if(raindrop.y + 64 < 0) {
+                    game.setScreen(new YouLost(game));
                 }
             }
-            if(raindrop.y + 64 < 0) {
-                iter.remove();
-                rainMusic.stop();
-                game.setScreen(new YouLost(game));
+        }else{
+            for (Iterator<Rectangle> iter = raindrops.iterator(); iter.hasNext(); ) {
 
+                Rectangle raindrop = iter.next();
+                raindrop.y -= speed;
+                if(raindrop.overlaps(bucket) && raindrop.y > ((bucket.height - 14) - speed)) {
+                    dropSound.play();
+                    iter.remove();
+                    contador++;
+                    if(contador % 10 == 0){
+                        speed *= 1.4f;
+                    }
+                }
+                if(raindrop.y + 64 < 0) {
+                    iter.remove();
+                    rainMusic.stop();
+                    game.setScreen(new YouLost(game));
+                }
             }
-
-
         }
 
     }
@@ -143,6 +293,53 @@ public class GameScreen implements Screen {
         raindrop.height = 64;
         raindrops.add(raindrop);
         lastDropTime = TimeUtils.nanoTime();
+    }
+
+    private void spawnGoldenDrop(){
+        Rectangle goldenDrop = new Rectangle();
+        goldenDrop.x = MathUtils.random(0, 800-64);
+        goldenDrop.y = 480;
+        goldenDrop.width = 64;
+        goldenDrop.height = 64;
+        goldenDrops.add(goldenDrop);
+        lastDropTime = TimeUtils.nanoTime();
+    }
+
+    private void spawnPowerUp() {
+        Rectangle powerUpRect = new Rectangle();
+        powerUpRect.x = (float)(Math.random() * (800 - 64));
+        powerUpRect.y = 480;
+        powerUpRect.width = 64;
+        powerUpRect.height = 64;
+        powerUps.add(powerUpRect);
+        lastPowerUpTime = TimeUtils.nanoTime();
+        powerUpSpawned = true;
+    }
+
+    private void compAccio(int accio){
+        switch(accio){
+            case 1:
+                powerUpBucketBig = true;
+                activePowerUp = "Activat poder de cub gran";
+                break;
+            case 2:
+                powerUpBucketTiny = true;
+                activePowerUp = "Activat poder de cub petit";
+                break;
+            case 3:
+                powerUpInvertControl = true;
+                activePowerUp = "Activat poder de controls inversos";
+                break;
+            case 4:
+                powerUpGoldenDrop = true;
+                activePowerUp = "Activat poder de gotes daurades (Punts x5)";
+                break;
+        }
+        timePowerUp = TimeUtils.nanoTime();
+    }
+
+    private void resetPowerUpString(){
+        activePowerUp = "No hi ha cap poder equipat...";
     }
 
     @Override
--- core/src/com/ecabezue/drop/YouLost.java
+++ core/src/com/ecabezue/drop/YouLost.java
@@ -5,25 +5,49 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.scenes.scene2d.Event;
+import com.badlogic.gdx.scenes.scene2d.EventListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.Touchable;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.badlogic.gdx.scenes.scene2d.ui.Container;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class YouLost implements Screen {
 
     final Drop game;
+    private Stage stage;
 
     OrthographicCamera camera;
+    private Texture retry;
 
     Texture bg;
 
+    private Sprite photoGameOverRestart;
+
     private Skin skin;
-    private TextButton retry;
 
     public YouLost(final Drop game){
         this.game = game;
-        bg = new Texture(Gdx.files.internal("storm_background.jpg"));
+        stage = new Stage(new ScreenViewport());
+        Gdx.input.setInputProcessor(stage);
+        bg = new Texture(Gdx.files.internal("water-drop.jpg"));
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 800, 480);
+        retry = new Texture(Gdx.files.internal("retry.png"));
+
+        photoGameOverRestart = new Sprite(retry);
+        photoGameOverRestart.setSize(430, 160);
+        photoGameOverRestart.setPosition((int)(800 - (430 * 1.5)), 10);
     }
 
     @Override
@@ -35,19 +59,26 @@ public class YouLost implements Screen {
     public void render(float delta) {
         Gdx.gl.glClearColor(0, 0, 0.2f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
         camera.update();
         game.batch.setProjectionMatrix(camera.combined);
 
         game.batch.begin();
         game.batch.draw(bg, 0, 0, 800, 480);
-        game.introText.draw(game.batch, "¡Has perdido :(! ", 100, 150);
-        game.introText.draw(game.batch, "¡Toca en cualquier lugar para volver a la pantalla inicial!", 100, 100);
+        photoGameOverRestart.draw(game.batch);
+
+        game.introText.draw(game.batch, "¡Has perdido :(! ", 5, 50);
+        game.introText.draw(game.batch, "¡Pulsa el botón para reiniciar!", 5, 30);
 
         game.batch.end();
 
         if (Gdx.input.isTouched()) {
-            game.setScreen(new MainMenuScreen(game));
-            dispose();
+            Vector3 tmp=new Vector3(Gdx.input.getX(),Gdx.input.getY(), 0);
+            camera.unproject(tmp);
+            Rectangle textureBounds= new Rectangle(photoGameOverRestart.getX(),photoGameOverRestart.getY(),photoGameOverRestart.getWidth(),photoGameOverRestart.getHeight());
+            if(textureBounds.contains(tmp.x,tmp.y)) {
+                game.setScreen(new MainMenuScreen(game));
+            }
         }
     }
 
